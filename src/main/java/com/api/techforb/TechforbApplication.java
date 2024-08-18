789c1f0729901dc776dc811558d4e5ab385c361f
Marcos Corasaniti N <marcoscorasaniti07@gmail.com>
2024-08-18 11:32:01 -0300

    primer commit

---
+++ src/main/java/com/api/techforb/TechforbApplication.java
@@ -0,0 +1,13 @@
+package com.api.techforb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechforbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechforbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/techforb/TechforbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.techforb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechforbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
